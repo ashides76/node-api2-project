ca78bc815e71969a3e2ec5850477fc86090cc479
Ashish Desai <desai.ashish76@gmail.com>
2024-05-02 10:40:56 -0400

    added new get by id and post new posts method

--- api/server.js
+++ api/server.js
@@ -1,9 +1,18 @@
 // implement your server here
 const express = require('express')
+const postRouter = require('./posts/posts-router')
 
 const server = express()
 
-server.use(express.json())
 // require your posts router and connect it here
+server.use(express.json())
+server.use('/api/posts', postRouter)
+
+server.get('/', (req, res) => {
+    res.send(`
+      <h2>Posts API</h>
+      <p>Welcome to the Posts API</p>
+    `);
+  });
 
 module.exports = server
